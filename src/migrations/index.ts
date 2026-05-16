fdc5adb36521a07518db8606ce12a57ca302149c
surenfernand <fernandosuren6@gmail.com>
2026-05-17 01:46:37 +0530

    build files update

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260515_214622_sync_schema from './20260515_214622_sync_schema';
+import * as migration_20260516_201527 from './20260516_201527';
 
 export const migrations = [
   {
     up: migration_20260515_214622_sync_schema.up,
     down: migration_20260515_214622_sync_schema.down,
-    name: '20260515_214622_sync_schema'
+    name: '20260515_214622_sync_schema',
+  },
+  {
+    up: migration_20260516_201527.up,
+    down: migration_20260516_201527.down,
+    name: '20260516_201527'
   },
 ];
