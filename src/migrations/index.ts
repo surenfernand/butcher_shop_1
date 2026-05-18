8cb71d74dbfba57a9384a10446225b630c692023
surenfernand <fernandosuren6@gmail.com>
2026-05-18 17:55:33 +0530

    migrate issue fix

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20260515_214622_sync_schema from './20260515_214622_sync_schema';
-import * as migration_20260516_201527 from './20260516_201527';
+import * as migration_20260518_122150 from './20260518_122150';
 
 export const migrations = [
   {
-    up: migration_20260515_214622_sync_schema.up,
-    down: migration_20260515_214622_sync_schema.down,
-    name: '20260515_214622_sync_schema',
-  },
-  {
-    up: migration_20260516_201527.up,
-    down: migration_20260516_201527.down,
-    name: '20260516_201527'
+    up: migration_20260518_122150.up,
+    down: migration_20260518_122150.down,
+    name: '20260518_122150'
   },
 ];
