64c612b48414a83078d3d9e0e56c3002ad04697d
surenfernand <fernandosuren6@gmail.com>
2026-05-18 18:02:43 +0530

    git fix

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260518_122150 from './20260518_122150';
-
-export const migrations = [
-  {
-    up: migration_20260518_122150.up,
-    down: migration_20260518_122150.down,
-    name: '20260518_122150'
-  },
-];
